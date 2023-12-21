bfa04c2f105442266e0bb087899d23dfcab8028c
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2023-12-22 02:42:24 +0900

    release: 0.3.7

--- src/main.ts
+++ src/main.ts
@@ -2,28 +2,35 @@ import { MarkdownView, Notice, Plugin } from 'obsidian';
 import { createCalloutDecorator } from 'decorations';
 import { quoteInfoField } from 'quote-field';
 import { patchDecoration } from 'patch-widget-type';
+import { DEFAULT_SETTINGS, MathInCalloutSettingTab, MathInCalloutSettings } from 'settings';
 
 export default class MathInCalloutPlugin extends Plugin {
 	patchSucceeded: boolean;
+	notReadyNotice: Notice | null = null;
+	settings: MathInCalloutSettings;
 
 	async onload() {
+		await this.loadSettings();
+		await this.saveSettings();
+		this.addSettingTab(new MathInCalloutSettingTab(this));
+
 		this.patchSucceeded = false;
 
 		this.registerEditorExtension(quoteInfoField);
 
 		// Wait for a second to avoid showing the "You're not ready yet" notification when it's not necessary
-		let notReadyNotice: Notice;
-		this.app.workspace.onLayoutReady(() => setTimeout(() => {
-			if (!this.patchSucceeded) {
-				notReadyNotice = new Notice(`${this.manifest.name}: You're not ready yet. In Live Preview, type some math expression outside callouts.`, 0);
-			}
-		}, 1000));
+		this.app.workspace.onLayoutReady(() => setTimeout(() => this.showNotReadyNotice(), 1000));
 
 		patchDecoration(this, (builtInMathWidget) => {
 			// Wait for the view update to finish
 			setTimeout(() => {
-				if (notReadyNotice) notReadyNotice.hide();
-				new Notice(`${this.manifest.name}: You're ready!`, 1500);
+				if (this.notReadyNotice) {
+					this.notReadyNotice.hide();
+					this.notReadyNotice = null;
+					if (this.settings.notification) {
+						new Notice(`${this.manifest.name}: You're ready! (Note: this notifiction can be turned off in the plugin setting.)`, 1500);
+					}
+				}
 				this.registerEditorExtension(createCalloutDecorator(builtInMathWidget));
 				this.rerender()
 			}, 100);
@@ -39,5 +46,19 @@ export default class MathInCalloutPlugin extends Plugin {
 				leaf.view.setEphemeralState(eState);
 			}
 		});
+	}
+
+	showNotReadyNotice() {
+		if (!this.patchSucceeded && this.settings.notification) {
+			this.notReadyNotice = new Notice(`${this.manifest.name}: You're not ready yet. In Live Preview, type some math expression outside callouts.`, 0);
+		}
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
 	}
 }
---
+++ src/settings.ts
@@ -0,0 +1,38 @@
+import { PluginSettingTab, Setting } from 'obsidian';
+import MyPlugin from 'main';
+
+
+export interface MathInCalloutSettings {
+    notification: boolean;
+}
+
+export const DEFAULT_SETTINGS: MathInCalloutSettings = {
+    notification: true,
+};
+
+// Inspired by https://stackoverflow.com/a/50851710/13613783
+export type KeysOfType<Obj, Type> = NonNullable<{ [k in keyof Obj]: Obj[k] extends Type ? k : never }[keyof Obj]>;
+
+export class MathInCalloutSettingTab extends PluginSettingTab {
+	constructor(public plugin: MyPlugin) {
+		super(plugin.app, plugin);
+	}
+	
+	display(): void {
+		this.containerEl.empty();
+
+        new Setting(this.containerEl)
+            .setDesc('If something is not working, type some math expression outside callouts in Live Preview.')
+
+        new Setting(this.containerEl)
+            .setName("Show setup guidance notifications")
+			.addToggle((toggle) => {
+				toggle.setValue(this.plugin.settings.notification)
+					.onChange(async (value) => {
+						this.plugin.settings.notification = value;
+						await this.plugin.saveSettings();
+                        this.plugin.showNotReadyNotice();
+					});
+			});
+	}
+}
